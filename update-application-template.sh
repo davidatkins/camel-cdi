f9749f57131eeb35f73b3c18eec09572ba951fed
David Atkins <david.atkins@atos.net>
2015-03-05 15:40:27 +0000

    fixed integration tests

---
+++ update-application-template.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+alias osc="docker run --rm -i --entrypoint=osc --net=host openshift/origin:latest --insecure-skip-tls-verify"
+cat application-template.json | osc process -f - | osc update -f -
