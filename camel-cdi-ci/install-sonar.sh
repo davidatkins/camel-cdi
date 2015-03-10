24e575c44a10c1ab12ada75204a4d2063779c9b6
David Atkins <david.atkins@atos.net>
2015-03-10 15:37:11 +0000

    added sonar instance, fixed some osc command calls. sonar wip!

--- camel-cdi-ci/install-sonar.sh
+++ camel-cdi-ci/install-sonar.sh
@@ -2,4 +2,4 @@
 alias osc="docker run --rm -i --entrypoint=osc --net=host openshift/origin:v0.3.4 --insecure-skip-tls-verify"
 # the awk is used to dynamically set the DOCKER_REGISTRY ip as this tends to be different per install
 # TODO: could we setup an OpenShift route to remove this issue?
-awk '{while(match($0,"[$]{[^}]*}")) {var=substr($0,RSTART+2,RLENGTH -3);gsub("[$]{"var"}",ENVIRON[var])}}1' jenkins-config.json | osc create -f -
+awk '{while(match($0,"[$]{[^}]*}")) {var=substr($0,RSTART+2,RLENGTH -3);gsub("[$]{"var"}",ENVIRON[var])}}1' sonarqube-config.json | osc create -f -
